80dc5a019cf8ad2b9ed18d62c44de9cebc48e700
augusstt06 <augusstt06@gmail.com>
2024-09-07 19:17:25 +0900

    fix: ai form

--- src/app/_components/organisms/form/weather/weatherForm.tsx
+++ src/app/_components/organisms/form/weather/weatherForm.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 
-import DetailInfo from './childs/detailForm'
+import DetailForm from './childs/detailForm'
 import SummaryForm from './childs/summaryForm'
 
 export default function WeatherForm() {
@@ -16,7 +16,7 @@ export default function WeatherForm() {
         className={`row-start-2 row-span-7 col-start-2 col-span-5 fade-in ${isMounted ? 'opacity-100' : 'opacity-0'} space-y-6`}
       >
         <SummaryForm />
-        <DetailInfo />
+        <DetailForm />
       </div>
     </article>
   )
