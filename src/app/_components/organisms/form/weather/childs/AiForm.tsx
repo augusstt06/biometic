55182be29ea8ab619e92fca8baef01c2fb38a989
augusstt06 <augusstt06@gmail.com>
2024-09-08 16:06:19 +0900

    fix: ai form

--- src/app/_components/organisms/form/weather/childs/AiForm.tsx
+++ src/app/_components/organisms/form/weather/childs/AiForm.tsx
@@ -1,12 +1,24 @@
-// import { useState } from 'react'
+import { type ChangeEvent, useState } from 'react'
 
 import Button from '@/app/_components/atoms/button/Button'
 import LabelInput from '@/app/_components/molecules/input/LabelInput'
+import { fetchOpenAi } from '@/app/_modules/api'
 import { useAiStore } from '@/app/_store/ai'
 
 export default function AiForm() {
   const { simpleResponse, category } = useAiStore()
-  //   const [input, setInput] = useState<string>('')
+  const [inputValue, setInputValue] = useState<string>('')
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value)
+  }
+
+  const clickHandler = () => {
+    if (inputValue.trim() === '') {
+      alert('입력란을 채워주세요!')
+      return
+    }
+    void fetchOpenAi(inputValue)
+  }
   const seperateString = (msg: string | undefined) => {
     if (msg === undefined) return []
     return msg.split(/(?=\d\.\s)/)
@@ -17,10 +29,15 @@ export default function AiForm() {
         return (
           <div className="sort-col-flex space-y-2  h-full w-2/3 relative rounded-lg">
             <div className="absolute top-44 w-2/3 sort-row-flex justify-center space-x-3">
-              <LabelInput title="search" className="w-2/3" />
+              <LabelInput
+                title="search"
+                className="w-2/3"
+                onChange={changeHandler}
+              />
               <Button
                 title="검색"
                 className="bg-[#588ac0] px-4 py-3 rounded-lg hover:bg-[#476e99] simple-transition"
+                onClick={clickHandler}
               />
             </div>
           </div>
